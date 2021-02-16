8e4b12a76ae87543a02a1fd37ff04a6a71659c97
nikolai <nikolai@debian-x230.x230>
2021-02-16 20:59:57 +0300

    Fix no special treatment for windows

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -34,9 +34,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	/*
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },*/
-	{ NULL,       NULL,       NULL,       0,	    False, 	 -1 },
+	 * Rules have to have at least one item.
+	 * NULL class would match every single window,
+	 * to avoid this matching class is set to an empty string
+	 */
+	{ "",	      NULL,	  NULL,	      0,	    0, 		 -1 },
 };
 
 /* layout(s) */
