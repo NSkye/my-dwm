e4cec3c253b121d4769bf95b16925dde07a9925d
nikolai <nikolai@debian-x230.x230>
2021-02-28 15:39:42 +0300

    Add alpha patch

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -22,10 +22,17 @@ static const char col_gray3[]       = "#e2d8c2";
 static const char col_gray4[]       = "#eeeeee";
 // active tag color and current window border color
 static const char col_cyan[]        = "#e57504";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
